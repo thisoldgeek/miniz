7f5772fe90206df8b4202b6dfdf1dec2c024caa9
Bob Murphy <thisoldgeek@gmail.com>
2017-11-20 16:53:35 -0800

    changed command order zdial service

--- scripts/zdial.sh
+++ scripts/zdial.sh
@@ -2,5 +2,5 @@
 cd /home/pi/miniz
 sudo cat miniz_dial_service.txt > /etc/systemd/system/zdial.service
 sudo chmod 644 /etc/systemd/system/zdial.service
-sudo systemctl enable zdial
 sudo systemctl daemon-reload
+sudo systemctl enable zdial
