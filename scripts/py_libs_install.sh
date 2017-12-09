2b4712daf9c6e512caa28ae7ae0357840f8c81cf
Bob Murphy <thisoldgeek@gmail.com>
2017-12-08 18:33:56 -0800

    fixed line 607 player.py err with cp of good file

--- scripts/py_libs_install.sh
+++ scripts/py_libs_install.sh
@@ -8,6 +8,7 @@ sudo pip3 install pi3d
 sudo apt-get install python3-pil
 sudo apt-get install python3-setuptools 
 sudo pip3 install  pylms
+sudo cp player.py /usr/local/lib/python3.5/dist-packages/pylms/player.py
 sudo apt-get install python3-numpy 
 sudo pip3 install RPi.GPIO        
 
